0c0850af873442358455a7f352b981cc0832b34a
Drew Wyatt <drew.j.wyatt@gmail.com>
2023-04-04 20:13:01 -0400

    it works?

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { RGB } from './theme'
-import { dot } from './utils'
+import { Point, Draw, toMidpoint, randomizer } from './utils'
 
 const canvas = document.querySelector('canvas')!
 const ctx = canvas.getContext('2d')!
@@ -7,18 +7,33 @@ const ctx = canvas.getContext('2d')!
 ctx.fillStyle = RGB.Foreground
 ctx.strokeStyle = RGB.Foreground
 
-type Point = [x: number, y: number]
-
 const width = 800
 const height = 800
 const padding = 20
 
-const points = [
+const vertexPoints = [
   [width / 2, padding],
   [padding, height - padding],
   [width - padding, height - padding],
 ] satisfies Point[]
+const getRandomVertex = randomizer(vertexPoints)
 
-for (const point of points) {
-  dot(ctx, point)
+const startingPoint: Point = [400, 400] // TODO: make this random
+
+for (const point of vertexPoints) {
+  Draw.dot(ctx, point)
 }
+
+const wait = (time: number) => new Promise(res => setTimeout(res, time))
+
+const run = async () => {
+  let cursor = startingPoint
+  for (let i = 0; i < 1000000; i++) {
+    const vertex = getRandomVertex()
+    cursor = toMidpoint(cursor, vertex)
+    Draw.dot(ctx, cursor)
+    await wait(10)
+  }
+}
+
+run()
--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,27 @@
 import type { Color } from './theme'
 export type Context = CanvasRenderingContext2D
+export type Point = [x: number, y: number]
 
 type DotOptions = {
   color: Color
 }
 
-export const dot = (
-  ctx: Context,
-  [x, y]: [x: number, y: number],
-  options: Partial<DotOptions> = {},
-) => {
-  const prevStyle = ctx.fillStyle
-  if (options.color) {
-    ctx.fillStyle = options.color
-  }
+export namespace Draw {
+  export const dot = (ctx: Context, [x, y]: Point, options: Partial<DotOptions> = {}) => {
+    const prevStyle = ctx.fillStyle
+    if (options.color) {
+      ctx.fillStyle = options.color
+    }
 
-  ctx.fillRect(x, y, 4, 4)
-  ctx.fillStyle = prevStyle
+    ctx.fillRect(x, y, 4, 4)
+    ctx.fillStyle = prevStyle
+  }
 }
+
+export const toMidpoint = (a: Point, b: Point): Point => [
+  a[0] + (b[0] - a[0]) * 0.5,
+  a[1] + (b[1] - a[1]) * 0.5,
+]
+
+export const randomizer = (vertexPoints: Point[]) => () =>
+  vertexPoints[Math.floor(Math.random() * vertexPoints.length)]
